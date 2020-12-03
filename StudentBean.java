423cb0ba4fc1737ab2fb824e848082216b743714
Nitish Choudhary <52026663+Bramha007@users.noreply.github.com>
2020-12-03 21:43:49 +0530

    Add files via upload

---
+++ StudentBean.java
@@ -0,0 +1,70 @@
+package com.bean;
+
+
+public class StudentBean {
+	private String studentId;
+	private String studentName;
+	private String gender;
+	private String dob;
+	private String city;
+	private String state;
+	private String email;
+	private String qualification;
+	private String stream;
+
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	public String getStream() {
+		return stream;
+	}
+	public void setStream(String stream) {
+		this.stream = stream;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+}
